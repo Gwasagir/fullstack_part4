7c5614b7f8f244e8b598e46c1784d2694d00128c
Niko Keurulainen <keurulainen.niko@gmail.com>
2023-06-17 14:17:39 +0300

    Done 4.4

--- backend/tests/list_helper.test.js
+++ backend/tests/list_helper.test.js
@@ -1,22 +1,54 @@
 const listHelper = require('../utils/list_helper')
+const blogs_for_test = require('../misc/blogs_for_test')
 
-test('dummy returns one empty array', () => {
-  const blogs = []
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+describe('dummy returns', () => {
+  test('one from empty array', () => {
+    const blogs = []
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+  })
+
+  test('one from string', () => {
+    const result = listHelper.dummy('test string')
+    expect(result).toBe(1)
+  })
+
+  test('one from number', () => {
+    const result = listHelper.dummy(10)
+    expect(result).toBe(1)
+  })
+
+  test('one from empty', () => {
+    const result = listHelper.dummy()
+    expect(result).toBe(1)
+  })
 })
 
-test('dummy returns one string', () => {
-  const result = listHelper.dummy('test string')
-  expect(result).toBe(1)
-})
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
 
-test('dummy returns one number', () => {
-  const result = listHelper.dummy(10)
-  expect(result).toBe(1)
-})
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
 
-test('dummy returns one empty', () => {
-  const result = listHelper.dummy()
-  expect(result).toBe(1)
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    console.log('listWithOneBlog.likes : ',listWithOneBlog[0].likes)
+    expect(result).toBe(listWithOneBlog[0].likes)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(blogs_for_test.blogs)
+    expect(result).toBe(36)
+  })
 })
--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = () => {
   return 1
 }
 
+const totalLikes = blogs => {
+  const result = blogs.reduce(function(sumLikes, blogPost) {
+    return sumLikes + blogPost.likes
+  }, 0)
+  return blogs.length === 0
+    ? 0
+    : result
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
