c37eee9c1ea6abededce7efdfaba6b690047e8a9
Niko Keurulainen <keurulainen.niko@gmail.com>
2023-06-19 09:40:53 +0300

    Middlewares activated / taken to use

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,13 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
-const blogRouter = require('./controllers/blog')
-const userRouter = require('./controllers/user')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+
+const blogRouter = require('./controllers/blog')
+const userRouter = require('./controllers/user')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -19,9 +22,13 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
